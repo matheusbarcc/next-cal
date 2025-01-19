7686e5988b34532337c2bd403aa820ba25720508
matheusbarcc <matheusbarcon@gmail.com>
2025-01-19 14:25:03 -0300

    style: add time intervals page visual structure

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,60 @@
+import {
+  Heading,
+  MultiStep,
+  Button,
+  Text,
+  Checkbox,
+  TextInput,
+} from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import {
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+  IntervalsBox,
+  IntervalsContainer,
+} from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá</Heading>
+        <Text>
+          Defina o intervalo de horários que você está disponível em cada dia da
+          semana.
+        </Text>
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+
+      <IntervalsBox as="form">
+        <IntervalsContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Terça-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalsContainer>
+        <Button type="submit">
+          Próximo passo <ArrowRight weight="bold" />
+        </Button>
+      </IntervalsBox>
+    </Container>
+  )
+}
